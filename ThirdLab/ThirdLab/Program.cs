60b57792a76d6ca6bb4859bea542c7252931e6f2
an2onsol <74258164+an2onsol@users.noreply.github.com>
2021-05-14 15:12:14 +0300

    fix

--- ThirdLab/ThirdLab/Program.cs
+++ ThirdLab/ThirdLab/Program.cs
@@ -5,7 +5,7 @@ namespace ThirdLab {
         static void Main(string[] args) {
             Factory CocaCola = new Factory("ул. Островского", 1000, DateTime.Now, 3, 15, TypesOfBuilding.Industrial, "Me", 10000, 1220000);
             Console.WriteLine(CocaCola.ToString());
-            MilkFactory Savushkin = new MilkFactory("ул. Ленина", 1000, DateTime.Now, 3, 15, TypesOfBuilding.Industrial, "Me", 140, 12310000, 10);
+            Factory Savushkin = new MilkFactory("ул. Ленина", 1000, DateTime.Now, 3, 15, TypesOfBuilding.Industrial, "Me", 140, 12310000, 10);
             Console.WriteLine(Savushkin.ToString());
             MeatFactory Omkk = new MeatFactory("ул. Пушкина", 1000, DateTime.Now, 3, 15, TypesOfBuilding.Industrial, "Me", 12000, 12312312, 10, 200);
             Console.WriteLine(Omkk.ToString());
